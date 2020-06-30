1931caa31e73b1c480991980b20847504cd63029
Matt075 <66984978+Matt075@users.noreply.github.com>
2020-06-30 10:44:11 -0400

    Add files via upload

---
+++ Client.c
@@ -0,0 +1,150 @@
+/*
+** a simple web browser
+** compile: cc -o client client-single.c
+** run: ./client HOST HTTPPORT webpage
+**
+** the server is supposed to start on port 4000 and
+** make sure the server has been started before this.
+**
+** Basic steps for client to communicate with a servr
+** 1: get a socket
+** 2: connect to the server using ip and port
+** 3: read and/or write as many times as needed
+** 4: close the socket.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define BUFFERSIZE 256 
+
+int get_server_connection(char *hostname, char *port);
+void compose_http_request(char *http_request, char *filename);
+void web_browser(int http_conn, char *http_request);
+void print_ip( struct addrinfo *ai);
+
+int main(int argc, char *argv[])
+{
+    int http_conn;  
+    char http_request[BUFFERSIZE];
+
+    if (argc != 4) {
+        printf("usage: client HOST HTTPORT webpage\n");
+        exit(1);
+    }
+
+    // steps 1, 2: get a connection to server
+    if ((http_conn = get_server_connection(argv[1], argv[2])) == -1) {
+       printf("connection error\n");
+       exit(1);
+    }
+
+    // step 3.0: make an HTTP request
+    compose_http_request(http_request, argv[3]);
+
+    // step 3: web browser, send to/receive from the server
+    web_browser(http_conn, http_request);
+
+    // step 4: as always, close the socket when done
+    close(http_conn);
+}
+
+int get_server_connection(char *hostname, char *port) {
+    int serverfd;
+    struct addrinfo hints, *servinfo, *p;
+    int status;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = PF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+   if ((status = getaddrinfo(hostname, port, &hints, &servinfo)) != 0) {
+       printf("getaddrinfo: %s\n", gai_strerror(status));
+       return -1;
+    }
+
+    print_ip(servinfo);
+    for (p = servinfo; p != NULL; p = p ->ai_next) {
+       // Step 1: get a socket
+       if ((serverfd = socket(p->ai_family, p->ai_socktype,
+                           p->ai_protocol)) == -1) {
+           printf("socket socket \n");
+           continue;
+       }
+
+       // Step 2: connect to the server
+       if ((status = connect(serverfd, p->ai_addr, p->ai_addrlen)) == -1) {
+           close(serverfd);
+           printf("socket connect \n");
+           continue;
+       }
+       break;
+    }
+
+    freeaddrinfo(servinfo);
+   
+    if (status != -1) return serverfd;
+    else return -1;
+}
+
+void  compose_http_request(char *http_request, char *filename) {
+    strcpy(http_request, "GET /");
+    strcpy(&http_request[5], filename);
+    strcpy(&http_request[5+strlen(filename)], " ");
+}
+
+void web_browser(int http_conn, char *http_request) {
+    int numbytes = 0;
+    char buf[256];
+    // step 3.1: send the HTTP request
+    send(http_conn, http_request, strlen(http_request), 0);
+
+    // step 3.2: receive message from server
+    while ((numbytes=recv(http_conn, buf, sizeof(buf)-1,  0)) > 0) {
+       if ( numbytes < 0)  {
+          perror("recv");
+          exit(1);
+        }
+
+       // The received may not end with a '\0' 
+       buf[numbytes] = '\0';
+       printf("%s",buf);
+    }
+}
+
+void print_ip( struct addrinfo *ai) {
+   struct addrinfo *p;
+   void *addr;
+   char *ipver;
+   char ipstr[INET6_ADDRSTRLEN];
+   struct sockaddr_in *ipv4;
+   struct sockaddr_in6 *ipv6;
+   short port = 0;
+
+   for (p = ai; p !=  NULL; p = p->ai_next) {
+      if (p->ai_family == AF_INET) {
+         ipv4 = (struct sockaddr_in *)p->ai_addr;
+         addr = &(ipv4->sin_addr);
+         port = ipv4->sin_port;
+         ipver = "IPV4";
+      }
+      else {
+         ipv6= (struct sockaddr_in6 *)p->ai_addr;
+         addr = &(ipv6->sin6_addr);
+         port = ipv4->sin_port;
+         ipver = "IPV6";
+      }
+      inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+      printf("serv ip info: %s - %s @%d\n", ipstr, ipver, ntohs(port));
+   }
+}
+      
